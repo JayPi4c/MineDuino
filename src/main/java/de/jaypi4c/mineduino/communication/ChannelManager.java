347410bc54d502334b6c3b107ea0e3fec49c86ce
Jonas Pohl <jonas.jp4c@gmail.com>
2025-12-18 18:43:29 +0100

    Updating to main branch of jduino

--- src/main/java/de/jaypi4c/mineduino/communication/ChannelManager.java
+++ src/main/java/de/jaypi4c/mineduino/communication/ChannelManager.java
@@ -1,6 +1,5 @@
 package de.jaypi4c.mineduino.communication;
 
-import org.schlunzis.jduino.channel.Channel;
 import org.schlunzis.jduino.channel.ChannelMessageListener;
 import org.schlunzis.jduino.channel.serial.SerialDevice;
 import org.schlunzis.jduino.channel.serial.SerialDeviceConfiguration;
@@ -9,7 +8,7 @@ import org.schlunzis.jduino.simple.SimpleChannel;
 
 public class ChannelManager {
 
-    public static final SimpleChannel simpleChannel = Channel.builder().simple().build();
+    public static final SimpleChannel simpleChannel = SimpleChannel.create();
 
     public static void init() {
         SerialDevice device = new SerialDevice("Super duper fast serial", "/dev/ttyACM0");
